8ddca799c34241fbe4d62b14a204c64213c66e4a
brusdamson@gmail.com <brusdamson@gmail.com>
2022-04-02 15:10:28 +0300

    1

---
+++ Hacaton_12/Hacaton_12/ViewModel/StartPageListViewModel.cs
@@ -0,0 +1,51 @@
+﻿using Hacaton_12.Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Text;
+using System.Windows.Input;
+using Xamarin.Forms;
+
+namespace Hacaton_12.ViewModel
+{
+    internal class StartPageListViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+        public ObservableCollection<Picture> Pictures { get; set; }
+
+        #region cmd
+        public ICommand OpenPictureCommand { get; protected set; }
+        public ICommand SaveCommand { get; protected set; }
+        public ICommand BackCommand { get; protected set; }
+        #endregion
+        StartPageViewModel selectedImage;
+        public StartPageListViewModel()
+        {
+            Pictures = new ObservableCollection<Picture>();
+            //OpenPictureCommand = new Command(OpenPicture);
+            //SaveCommand = new Command(SavePicture);
+            //BackCommand = new Command(Back);
+        }
+
+        StartPageViewModel SelectedImage
+        {
+            get { return selectedImage; }
+            set
+            {
+                if (selectedImage != value)
+                {
+                    StartPageViewModel tempView = value;
+                    selectedImage = null;
+                    OnPropertyChanged(nameof(SelectedImage));
+                    //Application.Current.MainPage.Navigation.PushAsync();
+                }
+            }
+        }
+        protected void OnPropertyChanged(string propName)
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(propName));
+        }
+    }
+}
