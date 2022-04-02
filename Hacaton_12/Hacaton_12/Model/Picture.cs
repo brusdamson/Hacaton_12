6b9d62fa6334040cbe3581308ad2a5ef8c4c5fda
brusdamson@gmail.com <brusdamson@gmail.com>
2022-04-02 15:17:04 +0300

    Revert "Revert "1""
    This reverts commit 76c005e654ad6d2ac49b58b9996378c5e80a81fb.

---
+++ Hacaton_12/Hacaton_12/Model/Picture.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Hacaton_12.Model
+{
+    internal class Picture
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
